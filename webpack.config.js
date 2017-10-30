a33f5cf59b3662b65edd7ef25be1a01ad5454a00
4guti <4guti@posteo.net>
2017-10-30 21:52:26 +0100

    Add Webpack and .gitignore

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+// https://nodejs.org/api/path.html
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Import our plugin -> ADDED IN THIS STEP
+
+const paths = {
+  DIST: path.resolve(__dirname, 'dist'),
+  SRC: path.resolve(__dirname, 'src'),
+  JS: path.resolve(__dirname, 'src/js'),
+};
+
+// Webpack configuration
+module.exports = {
+  entry: path.join(paths.JS, 'app.js'),
+  output: {
+    path: paths.DIST,
+    filename: 'app.bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.SRC, 'index.html'),
+    }),
+  ]
+};
